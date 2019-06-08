7d3e29fbf0be6b74d617c3b0f17f90b74141b098
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-06-08 17:31:00 +0530

    created models for the blog app

--- blog_website_project/blogapp/models.py
+++ blog_website_project/blogapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone 
+
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=256)
+    image = models.ImageField(upload_to="images/")
+    description = models.TextField()
+    category = models.OneToOneField(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(timezone.now)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) 
+    
+    def __str__(self):
+        return self.title
