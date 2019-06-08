a939e90b7d246f7c0d88103b26c65dd7ad4d52d8
Abhishek Pathak <pathakabhishek0003@gmail.com>
2019-06-08 18:26:19 +0530

    added registrationform and templates

---
+++ blog_website_project/blogapp/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from django.contrib.auth.forms import (UserCreationForm, 
+                                       AuthenticationForm
+                                      )
+from django.contrib.auth.models import User
+
+class RegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    firstname = forms.CharField(required=True)
+    lastname = forms.CharField(required=True)
+    class Meta:
+        model = User
+        fields = (
+            'firstname',
+            'lastname',
+            'username',
+            'email',
+            'password1',
+            'password2'
+        )
+
+    def save(self, commit=True):
+        user = super(RegistrationForm, self).save(commit=False)
+        user.firstname = self.cleaned_data['firstname']
+        user.lastname  = self.cleaned_data['lastname']
+        user.username  = self.cleaned_data['username']
+        user.email   = self.cleaned_data['email']
+
+        if commit:
+            user.save()
+            return user
+
--- blog_website_project/blogapp/views.py
+++ blog_website_project/blogapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Blog
+
 
 # Create your views here.
+
+def homepage(request):
+    return render(request, 'blogapp/homepage.html',
+                 context={'blog':Blog.objects.all()})
